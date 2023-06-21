ad102d0737438fdc4ffe09463d47dd1c3d89581d
Hirwa-Ghislain <ghislainhirwa11@gmail.com>
2023-06-21 12:18:10 +0200

    first commit

---
+++ server/models/todoItems.js
@@ -0,0 +1,12 @@
+//import mongoose to create new schema
+
+const mongoose = require('mongoose')
+
+const TodoItemSchema = new mongoose.Schema({
+    item:{
+        type:String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('todo',TodoItemSchema)
